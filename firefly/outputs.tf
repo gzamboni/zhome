4981ae5f3b3e0db2e551c7d2121d78e5e510d8e1
Giovani Zamboni <gzamboni@gmail.com>
2025-08-16 00:44:24 -0300

    feat: remove MySQL configuration and resources from Firefly module

--- firefly/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "mysql_service_name" {
-  description = "Name of the MySQL service"
-  value       = kubernetes_service.mysql.metadata[0].name
-}
-
-output "mysql_service_port" {
-  description = "Port of the MySQL service"
-  value       = kubernetes_service.mysql.spec[0].port[0].port
-}
